61a933faa4409377a6a460e4f5aa047ab1b2ce58
LongXiangL <bfh1104@gmail.com>
2023-06-22 11:36:05 +0800

    feat: add index page

---
+++ controllers/product-controller.js
@@ -0,0 +1,6 @@
+const productController = {
+  getProducts: (req, res) => {
+    return res.render('products')
+  }
+}
+module.exports = productController
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const productController = require('../controllers/product-controller')
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+router.get('/products', productController.getProducts)
+
+router.use('/', (req, res) => res.redirect('/products'))
 
 module.exports = router
