cb0f40f6d4c9c4d60f3d9d93054f1ffdccd316bc
LongXiangL <bfh1104@gmail.com>
2023-06-22 11:55:10 +0800

    feat: add admin index page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const productController = require('../controllers/product-controller')
+const admin = require('./modules/admin')
 
+router.use('/admin', admin)
 router.get('/products', productController.getProducts)
 
 router.use('/', (req, res) => res.redirect('/products'))
---
+++ routes/modules/admin.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+const adminController = require('../../controllers/admin-controller')
+router.get('/products', adminController.getProducts)
+router.use('/', (req, res) => res.redirect('/admin/products'))
+module.exports = router
