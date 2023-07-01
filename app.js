782cd5aa4a71187d080482fe64d2290843ef66fa
LongXiangL <bfh1104@gmail.com>
2023-07-02 00:43:18 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const handlebars = require('express-handlebars')
 const routes = require('./routes')
 const app = express()
 const faker = require('faker')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))// 樣板引擎，指定副檔名為 .hbs
 
 app.set('view engine', 'hbs')// 設定使用 Handlebars 做為樣板引擎
 app.use(express.urlencoded({ extended: true }))// body-parser
+
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash()) // 掛載套件
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
+  res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  next()
+})
 app.use(routes)
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const admin = require('./modules/admin')
 const productController = require('../controllers/product-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
@@ -10,5 +11,6 @@ router.post('/signup', userController.signUp)
 router.get('/products', productController.getProducts)
 
 router.use('/', (req, res) => res.redirect('/products'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
