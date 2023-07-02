ef7106e017d8bfcb604f2749cae5ffa55c9952ac
LongXiangL <bfh1104@gmail.com>
2023-07-02 10:50:29 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const faker = require('faker')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
@@ -15,7 +16,9 @@ app.set('view engine', 'hbs')// 設定使用 Handlebars 做為樣板引擎
 app.use(express.urlencoded({ extended: true }))// body-parser
 
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
-app.use(flash()) // 掛載套件
+app.use(passport.initialize()) // 初始化 Passport
+app.use(passport.session()) // 啟動 session 功能
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
