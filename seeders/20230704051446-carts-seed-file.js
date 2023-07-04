b9f3f156996a88b1929a27c22fd92158ab21dd91
LongXiangL <bfh1104@gmail.com>
2023-07-04 13:18:41 +0800

    add carts&cartItem seeders

---
+++ seeders/20230704051446-carts-seed-file.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const { User } = require('../models')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = await User.findAll({ where: { role: 'user' } })
+
+    await queryInterface.bulkInsert('Carts',
+      Array.from({ length: 3 }).map((item, index) => ({
+        // id: index + 1,
+        UserId: users[Math.floor(Math.random() * users.length)].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })), {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Carts', null, {})
+  }
+}
---
+++ seeders/20230704051503-cartitems-seed-file.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { Cart, Product } = require('../models')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const products = await Product.findAll()
+    const carts = await Cart.findAll()
+
+    await queryInterface.bulkInsert('cartItems',
+      Array.from({ length: 10 }).map((item, index) => ({
+        // id: index + 1,
+        ProductId: products[Math.floor(Math.random() * 10)].id,
+        CartId: carts[Math.floor(Math.random() * 3)].id,
+        quantity: Math.floor(Math.random() * 5) + 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })), {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('cartItems', null, {})
+  }
+}
