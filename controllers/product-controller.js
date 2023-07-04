e6c96a00eb647c0bc5b7aa1ba198ec11b88cc528
LongXiangL <bfh1104@gmail.com>
2023-07-04 15:21:37 +0800

    feat: add pagination on products index page

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,17 +1,26 @@
 const { Product } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const productController = {
   getProducts: (req, res) => {
-    return Product.findAll({
+    // 分頁
+    const DEFAULT_LIMIT = 9
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+    return Product.findAndCountAll({
+      limit,
+      offset,
       nest: true,
       raw: true
     }).then(products => {
-      const data = products.map(r => ({
+      const data = products.rows.map(r => ({
         ...r,
         description: r.description.substring(0, 50)
       }))
       return res.render('products', {
-        products: data
+        products: data,
+        pagination: getPagination(limit, page, products.count)
       })
     })
   }
--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,12 +1,8 @@
 const dayjs = require('dayjs')
-function equal (a, b, options) {
-  if (a === b) {
-    return options.fn(this)
-  } else {
-    return options.inverse(this)
-  }
-}
+
 module.exports = {
-  equal,
-  currentYear: () => dayjs().year() // 取得當年年份作為 currentYear 的屬性值，並導出
+  currentYear: () => dayjs().year(),
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
