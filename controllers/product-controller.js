a0b794dc564a369e83a322d44de18dde5a8b0e8a
LongXiangL <bfh1104@gmail.com>
2023-07-04 14:59:25 +0800

    add products index page

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,6 +1,19 @@
+const { Product } = require('../models')
+
 const productController = {
   getProducts: (req, res) => {
-    return res.render('products')
+    return Product.findAll({
+      nest: true,
+      raw: true
+    }).then(products => {
+      const data = products.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50)
+      }))
+      return res.render('products', {
+        products: data
+      })
+    })
   }
 }
 module.exports = productController
