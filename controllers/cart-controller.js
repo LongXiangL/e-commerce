60115b7a044069a2abffe9deaed37d9d94c3e57b
LongXiangL <bfh1104@gmail.com>
2023-07-05 08:53:24 +0800

    modify get Cart controller

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -3,18 +3,22 @@ const { Cart, CartItem, Product } = require('../models')
 const cartController = {
   getCart: (req, res, next) => {
     return Cart.findOne({
-      include: 'cartProducts'
+      include: [{
+        model: Product,
+        as: 'cartProducts',
+        through: CartItem
+      }]
     })
       .then(cart => {
         const totalPrice = cart.cartProducts.length > 0 ? cart.cartProducts.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
 
         const cartProducts = cart.cartProducts.map(product => {
           const { id, name, quantity, price, image } = product
-          return { id, name, quantity, price, image }
+          const CartItem = product.CartItem.dataValues
+          return { id, name, quantity, price, image, CartItem }
         })
         res.locals.getCart = cartProducts
         res.locals.totalPrice = totalPrice
-        console.log('打印機', cartProducts, totalPrice)
 
         return next()
       })
--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -4,5 +4,10 @@ module.exports = {
   currentYear: () => dayjs().year(),
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
+  },
+  multiply: function (a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+      return a * b
+    }
   }
 }
